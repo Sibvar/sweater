f4db6208c96d5635add23f0bec261d40cc84476e
Aliaksandr <sibelius@tut.by>
2023-08-08 17:05:31 +0300

    add recaptcha

--- src/main/java/com/example/sweater/domain/User.java
+++ src/main/java/com/example/sweater/domain/User.java
@@ -20,9 +20,6 @@ public class User implements UserDetails {
     private String username;
     @NotBlank(message = "Password cannot be empty")
     private String password;
-    @Transient
-    @NotBlank(message = "Password confirmation cannot be empty")
-    private String password2;
     private boolean active;
     @Email(message = "E-mail is not correct")
     @NotBlank(message = "E-mail cannot be empty")
@@ -39,14 +36,6 @@ public class User implements UserDetails {
     }
 
     public User() {
-    }
-
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
     }
 
     public String getEmail() {
